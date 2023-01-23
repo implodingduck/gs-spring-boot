bc4f0ab01045d6ee535a2741c1f725398c4459c7
implodingduck <zsek320@gmail.com>
2023-01-23 09:48:01 -0600

    adding an environment variable print out

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,24 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Map;
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		Map<String, String> env = System.getenv();
+		StringBuilder sb = new StringBuilder("Greetings from Spring Boot!");
+		String state = (System.getenv("EUREKA_INSTANCE_INITIALSTATUS").equals("UP")) ? "Active" : "Staging";
+		sb.append(state);
+		sb.append("<hr />");
+		for (String envName : env.keySet()) {
+			sb.append(envName);
+			sb.append("=");
+			sb.append(env.get(envName));
+			sb.append(" ~~~~ ");
+		}
+		return sb.toString();
 	}
 
 }
